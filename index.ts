4ca7555f5db419c9e05f602650a924df2507bd35
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-09-28 13:04:08 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,30 @@
+import { ethers } from 'ethers'
+import * as dotenv from 'dotenv'
+import * as abi from './abi.json'
+
+const main = async () => {
+  dotenv.config()
+  const { NODE_URL, TREASURY_ADDR } = process.env
+
+  if (!NODE_URL || NODE_URL.length === 0)
+    throw Error('NODE_URL not set in .env file')
+  if (!TREASURY_ADDR || TREASURY_ADDR.length === 0)
+    throw Error('TREASURY_ADDR not set in .env file')
+
+  const provider = new ethers.providers.JsonRpcProvider(NODE_URL)
+  const contract = new ethers.Contract(TREASURY_ADDR, abi, provider)
+
+  console.log('signers: ', await contract.getOwners())
+
+  // notable events:
+  // ExecutionSuccess
+  // SignMsg
+  // SafeReceived
+  // AddedOwner
+  // SafeSetup
+
+  // TODO: parse events
+  // contract.queryFilter()
+}
+
+main()
