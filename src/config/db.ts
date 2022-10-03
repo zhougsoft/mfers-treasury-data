cc6f981c471671dc9248f395e0d42b8315e5c6c8
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-10-03 12:24:37 -0300

    sqlite db added and working

--- src/config/db.ts
+++
@@ -1,9 +0,0 @@
-import { Database } from 'sqlite'
-import * as sqlite from 'sqlite3'
-
-const db = new Database({
-  driver: sqlite.Database,
-  filename: ':memory:',
-})
-
-export default db
--- src/migrate-up.ts
+++ src/migrate-up.ts
@@ -1,3 +1,34 @@
-import db from './config/db'
+import * as sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
-console.log(db)
+// using sqlite - a promise-based wrapper for sqlite3 library
+// docs: https://www.npmjs.com/package/sqlite
+
+const main = async () => {
+  try {
+    sqlite3.verbose()
+    const db = await open<sqlite3.Database, sqlite3.Statement>({
+      filename: ':memory:',
+      driver: sqlite3.Database,
+    })
+
+    const q_CreateTable =
+      'CREATE TABLE stash (id INTEGER PRIMARY KEY, val CHAR);'
+
+    const q_SeedTable =
+      "INSERT INTO stash (val) VALUES ('hello'), ('hi'), ('how r u')"
+
+    await db.exec(q_CreateTable)
+    await db.exec(q_SeedTable)
+    const rowResult = await db.get('SELECT * FROM stash WHERE id = ?', [1])
+    const allRowsResult = await db.all('SELECT * FROM stash')
+
+    console.log({ rowResult, allRowsResult })
+
+    await db.close()
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
